bc9fb5236d7477046d8a5a31fba9f4cf8a71a91e
cory <pengfei6223@gmail.com>
2022-03-07 14:58:22 +0800

    finish chat client

--- IMClient/src/main/java/com/example/imclient/ChatClient.java
+++ IMClient/src/main/java/com/example/imclient/ChatClient.java
@@ -1,11 +1,83 @@
 package com.example.imclient;
 
+import com.example.common.codec.ProtobufDecoder;
+import com.example.common.codec.ProtobufEncoder;
+import com.example.imclient.handler.ExceptionHandler;
+import com.example.imclient.handler.LoginResponseHandler;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.concurrent.GenericFutureListener;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 @Slf4j
 @Service
 public class ChatClient {
 
+	@Value("${server.ip}")
+	private String host;
+
+	@Value("${server.port}")
+	private int port;
+
+	@Autowired
+	private LoginResponseHandler responseHandler;
+
+	@Autowired
+	private ExceptionHandler exceptionHandler;
+
+	private GenericFutureListener<ChannelFuture> connectedListeners;
+
+	private EventLoopGroup eventLoopGroup;
+
+	private Bootstrap bootstrap;
+
+	ChatClient() {
+		eventLoopGroup = new NioEventLoopGroup();
+		bootstrap = new Bootstrap();
+	}
+
+	@PostConstruct
+	public void connect() {
+		try {
+			bootstrap.group(eventLoopGroup);
+			bootstrap.channel(NioSocketChannel.class);
+			bootstrap.option(ChannelOption.SO_KEEPALIVE, true);
+			bootstrap.option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
+			bootstrap.remoteAddress(host, port);
+
+			bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+				@Override
+				protected void initChannel(SocketChannel ch) throws Exception {
+					ch.pipeline().addLast("decoder", new ProtobufDecoder());
+					ch.pipeline().addLast("encoder", new ProtobufEncoder());
+					ch.pipeline().addLast(responseHandler);
+					ch.pipeline().addLast(exceptionHandler);
+				}
+			});
+
+			log.info("client start to connect the server!");
+
+			ChannelFuture future = bootstrap.connect();
+			future.addListener(connectedListeners);
+		} catch (Exception e) {
+			log.error("client connect failed, " + e.getMessage());
+		}
+	}
+
+	public void close() {
+		eventLoopGroup.shutdownGracefully();
+	}
 
 }
